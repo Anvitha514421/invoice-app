0143491ce0f48e4c82db32fa934c7876ba39ad7b
Anvitha514421 <149704539+Anvitha514421@users.noreply.github.com>
2023-11-25 00:14:27 +0000

    Created files and class related to Customer folder and updated drawable xml files

--- app/src/main/java/com/invoice/app/ui/AppScreen.kt
+++ app/src/main/java/com/invoice/app/ui/AppScreen.kt
@@ -10,6 +10,11 @@ sealed class AppScreen(@StringRes val title: Int, @DrawableRes val icon: Int, va
         object Login : AppScreen(R.string.login, R.drawable.ic_app_logo, "login")
         object Signup : AppScreen(R.string.signup, R.drawable.ic_app_logo, "signup")
     }
+     object Customers : AppScreen(R.string.customers, R.drawable.ic_customers, "nav_customers") {
+        object Home : AppScreen(R.string.customers, R.drawable.ic_customers, "customers")
+        object ManageCustomer : AppScreen(R.string.add_customer, R.drawable.ic_customers, "manage_customer")
+    }
+
 
     object Logout : AppScreen(R.string.logout, R.drawable.ic_logout, "logout")
 }
