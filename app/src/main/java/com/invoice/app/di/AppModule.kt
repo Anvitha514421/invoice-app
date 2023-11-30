21ab1ce4f17ac0f97c2e193cc6144eadf9fc1f25
Anvitha514421 <149704539+Anvitha514421@users.noreply.github.com>
2023-11-30 16:53:36 +0000

    made changes to AppModule

--- app/src/main/java/com/invoice/app/di/AppModule.kt
+++ app/src/main/java/com/invoice/app/di/AppModule.kt
@@ -1,34 +1,24 @@
 package com.invoice.app.di
 
 import com.google.firebase.auth.FirebaseAuth
-<<<<<<< HEAD
-=======
-import com.google.firebase.firestore.FirebaseFirestore
-import com.invoice.app.data.*
-import com.invoice.app.data.auth.AuthRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import com.invoice.app.data.auth.AuthRepositoryImpl
+import com.invoice.app.data.*
 import com.invoice.app.data.home.CustomersRepository
 import com.invoice.app.data.home.CustomersRepositoryImpl
 import com.invoice.app.data.home.DashboardRepository
 import com.invoice.app.data.home.DashboardRepositoryImpl
 import com.invoice.app.data.home.InvoiceRepository
 import com.invoice.app.data.home.InvoiceRepositoryImpl
->>>>>>> d636427a8f2d7e5eec5a103d21702805d1d2b212
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-<<<<<<< HEAD
-import com.invoice.app.data.auth.AuthRepositoryImpl
-import com.invoice.app.data.*
-import com.invoice.app.data.home.CustomersRepository
-import com.invoice.app.data.home.CustomersRepositoryImpl
-import com.invoice.app.data.home.InvoiceRepository
-import com.invoice.app.data.home.InvoiceRepositoryImpl
+import com.invoice.app.data.home.MyBusinessRepository
+import com.invoice.app.data.home.MyBusinessRepositoryImpl
+//import com.invoice.app.data.home.TaxRepository
+//import com.invoice.app.data.home.TaxRepositoryImpl
 import com.google.firebase.firestore.FirebaseFirestore
 import com.invoice.app.data.auth.AuthRepository
-=======
->>>>>>> d636427a8f2d7e5eec5a103d21702805d1d2b212
 
 @InstallIn(SingletonComponent::class)
 @Module
@@ -43,21 +33,16 @@ class AppModule {
     @Provides
     fun provideAuthRepository(impl: AuthRepositoryImpl): AuthRepository = impl
 
+    @Provides
+    fun provideMyBusinessRepository(impl: MyBusinessRepositoryImpl): MyBusinessRepository = impl
 
     @Provides
     fun provideCustomersRepository(impl: CustomersRepositoryImpl): CustomersRepository = impl
 
-<<<<<<< HEAD
-    @Provides
-    fun provideInvoiceRepository(impl: InvoiceRepositoryImpl): InvoiceRepository = impl
-
-
-=======
 
     @Provides
     fun provideInvoiceRepository(impl: InvoiceRepositoryImpl): InvoiceRepository = impl
 
     @Provides
     fun provideDashboardRepository(impl: DashboardRepositoryImpl): DashboardRepository = impl
->>>>>>> d636427a8f2d7e5eec5a103d21702805d1d2b212
 }
